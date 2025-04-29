ae4c23140f6c2bf75d2b23861eacdf719f261064
Imantha7 <imanthapiyumal7@gmail.com>
2025-04-29 22:12:10 +0530

    installed tailwind css ✅

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,7 +1,9 @@
 export default function Test(){
     return(
         <div>
+            <div>
             <h1 className="text-red-500 text-4xl">Hellow.....</h1>
+            </div>
         </div>
     )
 }
